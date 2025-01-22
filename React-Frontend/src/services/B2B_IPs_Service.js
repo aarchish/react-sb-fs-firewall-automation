c064db50938c3f11a1efcf6d3fa7a222b3608aef
Aarchish Jindal <aarchish0@gmail.com>
2025-01-22 19:31:58 +0530

    updated api endpoints in react

--- React-Frontend/src/services/B2B_IPs_Service.js
+++ React-Frontend/src/services/B2B_IPs_Service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getAccessToken } from "./Okta_Service";
 
-const API_URL = `${import.meta.env.VITE_API_BASE_URL}/b2b_ips`;
+const API_URL = `${import.meta.env.VITE_API_BASE_URL}/api/b2b_ips`;
 
 export const getListOfB2B_IPs = async () => {
   try {
