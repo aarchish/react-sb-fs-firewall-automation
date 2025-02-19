750c7f0eaa7d32a42300e246faaf40b44a347383
Aarchish Jindal <aarchish0@gmail.com>
2025-02-19 14:55:57 +0530

    some changes to postgres container

--- Postgres-DB/init.sh
+++ Postgres-DB/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-set -e
+#set -e
 
 # Create the database using the psql command
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE DATABASE firewall_automation;
-EOSQL
+#psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+#    CREATE DATABASE firewall_automation;
+#EOSQL
