ddc4e0e22aa2698b85ced8e074aec7db4ebe3d66
Aarchish Jindal <aarchish0@gmail.com>
2025-02-20 15:31:55 +0530

    tyring to fix ngnix

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 # Substitute environment variables in the Nginx configuration template
-envsubst '${VM_PUBLIC_IP}' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${VM_PUBLIC_IP}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 
 # Create the SSL certificate files from environment variables
+mkdir -p /etc/nginx/ssl
 echo "$SSL_CERT" > /etc/nginx/ssl/nginx-selfsigned.crt
 echo "$SSL_KEY" > /etc/nginx/ssl/nginx-selfsigned.key
 
